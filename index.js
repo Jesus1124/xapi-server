ddf53642d05cd5bdc1e2f49c9eeeb86322cca41e
DESKTOP-VBK9136\Eric <eric@hope.com>
2021-04-16 08:24:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let fs = require("fs"),
+  unzip = require("unzipper");
+
+let express = require("express");
+let cors = require("cors");
+let os = require("os");
+let path = require("path");
+let request = require("request");
+let xmldoc = require("xmldoc");
+let app = express();
+app.use(cors());
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/contents", { dotFiles: "allow" }));
+
+app.get("/", (req, res) => {
+  let zipURL = req.query.url;
+  var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+  console.log(fullUrl);
+  zipURL = fullUrl.split("?");
+  zipURL.shift();
+  zipURL = zipURL.join("?").substr(4);
+  let idx1 = zipURL.indexOf("%2F") + 3,
+    idx2 = zipURL.indexOf(".zip");
+  let extractedDirName = zipURL.substr(idx1, idx2 - idx1);
+  let unzipPipe = unzip.Extract({
+    path: __dirname + `/contents/${extractedDirName}`,
+  });
+
+  function returnTo() {
+    let file = __dirname + `/contents/${extractedDirName}/tincan.xml`;
+
+    fs.readFile(file, function (err, data) {
+      let document = new xmldoc.XmlDocument(data);
+      let url = "";
+      document
+        .childNamed("activities")
+        .childrenNamed("activity")
+        .forEach((activityElement) => {
+          url = activityElement.childNamed("launch").val;
+        });
+      return res.redirect(`/player.html?url=${url}`);
+    });
+  }
+  if (fs.existsSync(__dirname + `/contents/${extractedDirName}`)) {
+    console.log("Already Exist");
+    return returnTo();
+  }
+
+  unzipPipe.on("close", function () {
+    return returnTo();
+  });
+
+  request(zipURL).pipe(unzipPipe);
+});
+
+app.listen(8000, function () {
+  console.log("SCORM server is running on port 8000!");
+});
---
+++ public/js/xAPI-player.js
@@ -0,0 +1,202 @@
+/*
+ * Make JSON safe for IE6
+ * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/JSON#Browser_compatibility
+ */
+if (!window.JSON) {
+  window.JSON = {
+    parse: function (sJSON) {
+      /*jslint evil: true */
+      return eval("(" + sJSON + ")");
+    },
+    stringify: function (vContent) {
+      var sOutput = "",
+        nId,
+        sProp;
+      if (vContent instanceof Object) {
+        if (vContent.constructor === Array) {
+          for (nId = 0; nId < vContent.length; nId += 1) {
+            sOutput += this.stringify(vContent[nId]) + ",";
+          }
+          return "[" + sOutput.substr(0, sOutput.length - 1) + "]";
+        }
+        if (vContent.toString !== Object.prototype.toString) {
+          return '"' + vContent.toString().replace(/"/g, "\\$&") + '"';
+        }
+        for (sProp in vContent) {
+          if (vContent.hasOwnProperty(sProp)) {
+            sOutput +=
+              '"' +
+              sProp.replace(/"/g, "\\$&") +
+              '":' +
+              this.stringify(vContent[sProp]) +
+              ",";
+          }
+        }
+        return "{" + sOutput.substr(0, sOutput.length - 1) + "}";
+      }
+      return typeof vContent === "string"
+        ? '"' + vContent.replace(/"/g, "\\$&") + '"'
+        : String(vContent);
+    },
+  };
+}
+
+/* Set up TinCanJS */
+var tincan = new TinCan({
+    recordStores: [
+      {
+        endpoint: Config.endpoint,
+        username: Config.authUser,
+        password: Config.authPassword,
+        allowFail: false,
+      },
+    ],
+  }),
+  PrototypesLauncher = {};
+
+// PrototypesLauncher.Activity = {
+//   id: "http://id.tincanapi.com/activity/tincan-prototypes/launcher",
+//   definition: {
+//     type: "http://id.tincanapi.com/activitytype/lms",
+//     name: {
+//       "en-US": "Tin Can Prototypes Launcher",
+//     },
+//     description: {
+//       "en-US":
+//         "A tool for launching the Tin Can prototypes. Simulates the role of an LMS in launching experiences.",
+//     },
+//   },
+// };
+// PrototypesLauncher.context = {
+//   contextActivities: {
+//     grouping: [
+//       {
+//         id: "http://id.tincanapi.com/activity/tincan-prototypes",
+//       },
+//     ],
+//     category: [
+//       {
+//         id: "http://id.tincanapi.com/recipe/tincan-prototypes/launcher/1",
+//         definition: {
+//           type: "http://id.tincanapi.com/activitytype/recipe",
+//         },
+//       },
+//       {
+//         id:
+//           "http://id.tincanapi.com/activity/tincan-prototypes/launcher-template",
+//         definition: {
+//           type: "http://id.tincanapi.com/activitytype/source",
+//           name: {
+//             "en-US": "Tin Can Launcher Template",
+//           },
+//           description: {
+//             "en-US": "A launch tool based on the Tin Can launch prototypes.",
+//           },
+//         },
+//       },
+//     ],
+//   },
+// };
+
+// tincan.sendStatement(
+//   {
+//     actor: {
+//       name: Config.actor.name,
+//       mbox: Config.actor.mbox,
+//     },
+//     verb: {
+//       id: "http://adlnet.gov/expapi/verbs/experienced",
+//       display: {
+//         "en-US": "experienced",
+//       },
+//     },
+//     object: PrototypesLauncher.Activity,
+//     context: {
+//       registration: Config.registration,
+//       contextActivities: PrototypesLauncher.context.contextActivities,
+//     },
+//   },
+//   function (err, xhr) {
+//     console.log("Send state", err);
+//     console.log("Send state", xhr);
+//   }
+// );
+
+$(document).ready(function () {
+  var PROTOTYPE_ENDPOINT = Config.endpoint;
+  var PROTOTYPE_AUTH =
+    "Basic " + Base64.encode(Config.authUser + ":" + Config.authPassword);
+  var PROTOTYPE_REGISTRATION = Config.registration || TinCan.Utils.getUUID();
+  var actor = { mbox: "mailto:espitaj26@gmail.com", name: "Jesus Espitia" };
+
+  let indexFile = window.location.href.split("url=")[1];
+
+  sendLaunchedStatement(
+    actor,
+    "http://id.tincanapi.com/activity/tincan-prototypes/golf-example",
+    PROTOTYPE_REGISTRATION
+  );
+
+  $("#content").attr(
+    "src",
+    `/Golf/${indexFile}?` +
+      "endpoint=" +
+      encodeURIComponent(PROTOTYPE_ENDPOINT) +
+      "&auth=" +
+      encodeURIComponent(PROTOTYPE_AUTH) +
+      "&actor=" +
+      encodeURIComponent(JSON.stringify(actor)) +
+      "&registration=" +
+      encodeURIComponent(PROTOTYPE_REGISTRATION)
+  );
+});
+
+function sendLaunchedStatement(actor, activityId, registration) {
+  tincan.sendStatement(
+    {
+      actor: actor,
+      verb: {
+        id: "http://adlnet.gov/expapi/verbs/launched",
+        display: {
+          "en-US": "launched",
+        },
+      },
+      object: {
+        id: activityId,
+      },
+      context: {
+        registration: registration,
+        contextActivities: {
+          grouping: [
+            {
+              id: "http://id.tincanapi.com/activity/tincan-prototypes",
+            },
+          ],
+          category: [
+            {
+              id: "http://id.tincanapi.com/recipe/tincan-prototypes/launcher/1",
+              definition: {
+                type: "http://id.tincanapi.com/activitytype/recipe",
+              },
+            },
+            {
+              id:
+                "http://id.tincanapi.com/activity/tincan-prototypes/launcher-template",
+              definition: {
+                type: "http://id.tincanapi.com/activitytype/source",
+                name: {
+                  "en-US": "Tin Can Launcher Template",
+                },
+                description: {
+                  "en-US":
+                    "A launch tool based on the Tin Can launch prototypes.",
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    function (err, xhr) {}
+  );
+}
